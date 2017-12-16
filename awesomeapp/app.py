2bea62964442bfc2ec9efaef2db7833004af4a9f
Girish Ranganathan <girish.ranganathan@solarwinds.com>
2017-12-16 00:13:27 -0500

    awesome app code

---
+++ awesomeapp/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from redis import Redis
+
+app = Flask(__name__)
+redis = Redis(host='database', port=6379)
+
+@app.route('/')
+def hello():
+    count = redis.incr('hits')
+    return 'Hello World 3.0! I have been seen {} times.\n'.format(count)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
